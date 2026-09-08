6e8cf096df63ede6f7e6c3647418cfe199bbf350
Harsh Paliwal <harshcscemitra@gmail.com>
2026-09-08 11:13:58 +0530

    feat: integrate Web Contact Picker API to pick contacts directly from phone address book on mobile

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Piplantri Election PWA Service Worker (Network-First Strategy for Instant Live Updates)
-const CACHE_NAME = 'piplantri-election-v7-pin-1198';
+const CACHE_NAME = 'piplantri-election-v8-contact-picker';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
